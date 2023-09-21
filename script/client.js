d3711c890a0227cbce9a5b2f2cf13f9bac82c1ec
Zakarya <zakaryacarroll.contact@gmail.com>
2023-09-20 19:37:40 -0700

    Move round_rect function to game.js

--- script/client.js
+++ script/client.js
@@ -16,33 +16,6 @@ function global_to_canvas(pos) {
 	return new vector(tempx, tempy);
 }
 
-function round_rect(ctx, x, y, width, height, radius, fill, stroke) {
-  if (typeof stroke != "undefined" ) {
-    stroke = true;
-  }
-  if (typeof radius === "undefined") {
-    radius = 5;
-  }
-	ctx.fillStyle = fill;
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.lineTo(x + width - radius, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-  ctx.lineTo(x + width, y + height - radius);
-  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-  ctx.lineTo(x + radius, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-  ctx.lineTo(x, y + radius);
-  ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.closePath();
-  if (stroke) {
-    ctx.stroke();
-  }
-  if (fill) {
-    ctx.fill();
-  }        
-}
-
 function resize_canvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
